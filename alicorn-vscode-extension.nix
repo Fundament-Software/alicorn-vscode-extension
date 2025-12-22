dadac12c40ecea87df61d9a7bfb0cc92181c54d3
Luna Nova <git@lunnova.dev>
2025-12-22 05:17:50 -0800

    alicorn-vscode-extension: cursed compat with vsix/zip breaking change
    Copying @ameertaweel's fix in nix-community/nix-vscode-extensions#164

--- alicorn-vscode-extension.nix
+++ alicorn-vscode-extension.nix
@@ -15,6 +15,20 @@ let
 
   src = ./.;
 
+  # Cursed: detect whether nixpkgs expects .vsix or .zip
+  # https://github.com/nix-community/nix-vscode-extensions/pull/164
+  dummyExt = vscode-utils.buildVscodeExtension {
+    pname = "";
+    version = "";
+    src = "";
+    vscodeExtUniqueId = "";
+    vscodeExtPublisher = "";
+    vscodeExtName = "";
+  };
+  dummyDeps = builtins.map (x: x.name) dummyExt.nativeBuildInputs;
+  isVsix = builtins.elem "unpack-vsix-setup-hook" dummyDeps;
+  fileExtension = if isVsix then "vsix" else "zip";
+
   vsix = buildNpmPackage {
     pname = "${pname}-vsix";
     inherit version src;
@@ -37,7 +51,7 @@ let
     postBuild = ''
       npm run compile
       mkdir -p $vsix
-      echo y | vsce package -o $vsix/${pname}.vsix
+      echo y | vsce package -o $vsix/${pname}.${fileExtension}
     '';
 
     preCheck = ''
@@ -49,7 +63,7 @@ in
 vscode-utils.buildVscodeExtension {
   inherit pname version vsix;
   name = "${pname}-${version}";
-  src = "${vsix}/${pname}.vsix";
+  src = "${vsix}/${pname}.${fileExtension}";
   vscodeExtUniqueId = "${publisher}.${pname}";
   vscodeExtPublisher = publisher;
   vscodeExtName = pname;
